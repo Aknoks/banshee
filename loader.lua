e4317cfb5d2c26078ff12829624029688a97ca1b
Aknoks <60153476+Aknoks@users.noreply.github.com>
2025-02-28 16:51:29 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,9 @@
+if game.GameId == 1087859240 then
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/40db47a67f36abb41c12726416589845.lua"))()
+  return
+end
+
+if game.GameId == 1359573625 then
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/157f311f0c03767c211de326c8a5f05f.lua"))()
+  return
+end
